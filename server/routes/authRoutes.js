b218e8691abe4841819a3c47538da7d0b4e47c32
Ariel <arielox.ag@gmail.com>
2022-02-08 15:15:16 -0300

    listo logout

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -12,7 +12,7 @@ const {
 // '/api/v1/auth'
 router.post('/register', register);
 router.post('/login', login);
-router.delete('/logout', logout);
+router.delete('/logout', authenticateUser, logout);
 router.post('/verify-email', verifyEmail);
 
 module.exports = router;
